2d3920c803a95d11124113bd845fd170097aa998
royce-reed <rjreed1@gmail.com>
2022-06-11 18:08:11 -0500

    fixed broken links

---
+++ database/config/config.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const {DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_HOST, DB_DIALECT } = process.env;
+
+module.exports = {
+  development: {
+    username: 'postgres',
+    password: 'postgres',
+    database: 'fern-herm',
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  },
+  test: {
+    username: 'postgres',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  },
+  production: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    host: DB_HOST,
+    dialect: DB_DIALECT
+  }
+};
--- database/connection.js
+++ database/connection.js
@@ -1,20 +1,5 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
-const {DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_HOST, DB_DIALECT, DB_PORT } = process.env;
-const sequelize = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
-  host: DB_HOST,
-  dialect: DB_DIALECT,
-  logging: false,
-});
-const { db } = require('./models/index.js');
-//const Sequelize = require('sequelize');
-require('dotenv').config();
-const { DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_HOST, DB_DIALECT } = process.env;
-// db.sequelize = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
-//   host: DB_HOST,
-//   dialect: DB_DIALECT,
-//   logging: false,
-// });
+
+const db = require('./models/index.js');
 
 db.sequelize.authenticate()
   .then(() => console.log('✨ Database Connected!'))
