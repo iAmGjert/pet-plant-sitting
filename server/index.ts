89fb8b71c5d8389f86659ac5f16d002d2db9ef58
royce-reed <rjreed1@gmail.com>
2022-06-11 09:46:00 -0500

    add morgan middleware

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,13 @@
-
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
 
 const app = express();
 
 const CLIENT_PATH = path.resolve(__dirname, '../client/build');
 app.use(express.static(CLIENT_PATH));
 app.use(express.json());
+app.use(morgan('tiny'));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
