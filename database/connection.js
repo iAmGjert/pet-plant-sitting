4b97753cde858d7066e1479906db64d2efb28a38
Raymond Jeong <raymondjjeong@gmail.com>
2022-06-11 15:47:29 -0500

    Set Up Local Database

--- database/connection.js
+++ database/connection.js
@@ -1,8 +1,9 @@
 const Sequelize = require('sequelize');
+require('dotenv').config();
 const {DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_HOST, DB_DIALECT, DB_PORT } = process.env;
-const sequelize = new Sequelize('fern-herm', DB_USERNAME, DB_PASSWORD, {
+const sequelize = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
   host: DB_HOST,
-  dialect: 'postgres',
+  dialect: DB_DIALECT,
   logging: false,
 });
 
