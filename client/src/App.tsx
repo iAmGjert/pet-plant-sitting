34422b2edd3bbe2b0a23bb580bfd718bdcaa62df
royce-reed <rjreed1@gmail.com>
2022-06-10 21:46:05 -0500

    eslint working

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,17 @@
+/* eslint-disable react/react-in-jsx-scope */
 import { FC } from 'react';
 
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface Props {
   
 }
  
 const App: FC<Props> = () => {
-
   return ( 
     <div>
-          <h1>Hello world</h1>
+      <h1>Hello world</h1>
     </div>
-   )
-}
+  );
+};
  
 export default App;
--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const express = require('express');
 const path = require('path');
 
@@ -5,11 +6,11 @@ const app = express();
 
 const CLIENT_PATH = path.resolve(__dirname, '../client/build');
 app.use(express.static(CLIENT_PATH));
-app.use(express.json())
+app.use(express.json());
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`
     Server is listening at http://localhost:${port}
   `);
-})
+});
