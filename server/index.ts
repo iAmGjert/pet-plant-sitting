edc84067270d3beb537ffe0d11421fe5cdc5d3c1
Braeden Ford <bford0002@gmail.com>
2022-06-14 14:58:14 -0500

    merge

--- server/index.ts
+++ server/index.ts
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const { db } = require('../database/index');
 const app = express();
 const authRouter = require('./routes/auth.ts');
-const passport2 = require('passport');
+const passport = require('passport');
 require('dotenv').config();
 require('./auth/passport.ts');
 
@@ -15,8 +15,8 @@ app.use(session({
   resave: true,
   saveUninitialized: true
 }));
-app.use(passport2.initialize());
-app.use(passport2.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 
@@ -38,3 +38,5 @@ app.listen(port, () => {
 db.authenticate()
   .then(() => console.log('🥂 Connected to database'))
   .catch((err: string) => console.error(err));
+
+export {};
