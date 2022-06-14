994b424dbba33fa014b3fe1858fc4af096dc59a1
Braeden Ford <bford0002@gmail.com>
2022-06-14 14:13:44 -0500

    auth working for most part

--- server/auth/passport.ts
+++ server/auth/passport.ts
@@ -1,6 +1,6 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 require('dotenv').config();
-const passport3 = require('passport');
+const passport = require('passport');
 // const User = require('../../database/models/Users.model.ts');
 const { User } = require('../../database/index.ts');
 
@@ -8,28 +8,33 @@ const { User } = require('../../database/index.ts');
 const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, DB_HOST, PORT, CLIENT_URL } = process.env;
 
 
-passport3.use(new GoogleStrategy({
-    clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: `${CLIENT_URL}:${PORT}/auth/google/callback`
-  },
-  (accessToken: any, refreshToken: any, profile: any, cb: any) => {
-    User.findOrCreate({ name: profile.displayName }, {
-      name: profile.displayName,
-      image: profile.picture
-
-    }, (err: any, user: any) => {
-      return cb(err, user);
+passport.use(new GoogleStrategy({
+  clientID: GOOGLE_CLIENT_ID,
+  clientSecret: GOOGLE_CLIENT_SECRET,
+  callbackURL: `${CLIENT_URL}:${PORT}/auth/google/callback`
+},
+(accessToken: any, refreshToken: any, profile: any, cb: any) => {
+  User.findOrCreate({ where: {name: profile.displayName}, defaults: {
+    name: profile.displayName,
+    image: profile.picture
+  } })
+    .then((user: any, err: any) => {
+      return cb(user, err);
+    })
+    .catch((err: any) => {
+      console.log(err);
     });
-  }
+}
 ));
 
-passport3.serializeUser((user: any, callback: any) => {
+passport.serializeUser((user: any, callback: any) => {
   callback(null, user.id);
 });
 
-passport3.deserializeUser((id: any, callback: any) => {
-  User.findById(id, (error, user) => {
+passport.deserializeUser((id: any, callback: any) => {
+  User.findById(id, (error: any, user: any) => {
     callback(error, user);
   });
 });
+
+export {};
