effd776a3e6248689961c5b22882cd6894bb755c
ibenyourbro <iben.oneal@gmail.com>
2022-06-14 14:57:46 -0500

    Passport Fixes

--- server/auth/passport.ts
+++ server/auth/passport.ts
@@ -4,37 +4,41 @@ const passport = require('passport');
 // const User = require('../../database/models/Users.model.ts');
 const { User } = require('../../database/index.ts');
 
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, DB_HOST, PORT, CLIENT_URL } =
+  process.env;
 
-const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, DB_HOST, PORT, CLIENT_URL } = process.env;
-
-
-passport.use(new GoogleStrategy({
-  clientID: GOOGLE_CLIENT_ID,
-  clientSecret: GOOGLE_CLIENT_SECRET,
-  callbackURL: `${CLIENT_URL}:${PORT}/auth/google/callback`
-},
-(accessToken: any, refreshToken: any, profile: any, cb: any) => {
-  User.findOrCreate({ where: {name: profile.displayName}, defaults: {
-    name: profile.displayName,
-    image: profile.picture
-  } })
-    .then((user: any, err: any) => {
-      return cb(user, err);
-    })
-    .catch((err: any) => {
-      console.log(err);
-    });
-}
-));
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: `${CLIENT_URL}:${PORT}/auth/google/callback`,
+    },
+    (accessToken: any, refreshToken: any, profile: any, cb: any) => {
+      User.findOrCreate({ where: { name: profile.displayName } })
+        .then((user: any) => {
+          return cb(null, user);
+        })
+        .catch((err: any) => {
+          console.log(err);
+        });
+    }
+  )
+);
 
 passport.serializeUser((user: any, callback: any) => {
-  callback(null, user.id);
+  callback(null, user);
 });
 
-passport.deserializeUser((id: any, callback: any) => {
-  User.findById(id, (error: any, user: any) => {
-    callback(error, user);
-  });
+// passport.deserializeUser((id: any, callback: any) => {
+//   User.findById(id)
+//     .then((user: any) => {
+//       callback(null, user);
+//     })
+//     .catch(callback);
+// });
+passport.deserializeUser((user: any, done: any) => {
+  done(null, user);
 });
 
 export {};
