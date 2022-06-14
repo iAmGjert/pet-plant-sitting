cad46b0b6d3d740da4950c1bd3530e57afa1fb01
Braeden Ford <bford0002@gmail.com>
2022-06-14 18:51:09 -0500

    merge

---
+++ client/src/state/features/map/mapSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+//import type { RootState } from '../../store';
+
+const initState = {
+  value: 'shiny piece of garbage'
+};
+
+export const userProfileSlice = createSlice({
+  name: 'userProile',
+  initialState: initState,
+  reducers: {
+    changeName: (state, action: PayloadAction<string>)=>{
+      state.value = action.payload;
+      return state;
+    },
+  }
+});
+
+export const { 
+  changeName 
+} = userProfileSlice.actions;
+
+export default userProfileSlice.reducer;
