47ba7e6a4600c4899726f414e83a8b39e5bf83dd
velouriagreen <sammy_soysauce@yahoo.com>
2022-06-15 13:15:42 -0500

    Added the imported css styling from react calendar documentation

--- client/src/Components/Calendar/Calendar.tsx
+++ client/src/Components/Calendar/Calendar.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 // import PropTypes from 'prop-types';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 const CalendarApp = () =>{
   const [value, onChange] = useState(new Date());
