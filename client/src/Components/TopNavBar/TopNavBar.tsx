f4e176dffc337be39241a559b0da206509de8acb
royce-reed <rjreed1@gmail.com>
2022-06-15 17:04:11 -0500

    added events button to top nav bar

--- client/src/Components/TopNavBar/TopNavBar.tsx
+++ client/src/Components/TopNavBar/TopNavBar.tsx
@@ -8,12 +8,14 @@ const TopNavBar = () => {
     <div>
       <Navbar bg='light' expand='lg'>
         <Container>
-          <Navbar.Brand href='#home'>Fern-Herm</Navbar.Brand>
+          <Navbar.Brand href='/'>Fern-Herm</Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='me-auto'>
-              <Nav.Link href='#home'>Home</Nav.Link>
-              <Nav.Link href='#link'>Link</Nav.Link>
+              <Nav.Link href='/'>Home</Nav.Link>
+
+
+              <Nav.Link href='/events'>Events</Nav.Link>
               <NavDropdown title='Dropdown' id='basic-nav-dropdown'>
                 <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
                 <NavDropdown.Item href='#action/3.2'>
