07214dcbf5622b5522b31d80a932e49654a6073a
Eric Gjertsen <gjertsen.eric@gmail.com>
2022-06-15 12:24:03 -0500

    added /jobs route to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import Login from './Pages/Login';
 import MapMain from './Pages/MapMain';
 import CalendarMain from './Pages/CalendarMain';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import JobsMain from './Pages/JobsMain';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface Props {}
@@ -17,6 +18,7 @@ const App: FC<Props> = () => {
         <Route path='/login' element={<Login />} />
         <Route path='/map' element={<MapMain />} />
         <Route path='/calendar' element={<CalendarMain />} />
+        <Route path='/jobs' element={<JobsMain />} />
       </Routes>
     </BrowserRouter>
   );
