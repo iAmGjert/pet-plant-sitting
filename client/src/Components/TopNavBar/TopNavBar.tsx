f4590c745646d47588c8932ab691d78deee59893
Eric Gjertsen <gjertsen.eric@gmail.com>
2022-06-15 13:20:56 -0500

    navbar links updated in top nav bar.

--- client/src/Components/TopNavBar/TopNavBar.tsx
+++ client/src/Components/TopNavBar/TopNavBar.tsx
@@ -8,23 +8,30 @@ const TopNavBar = () => {
     <div>
       <Navbar bg='light' expand='lg'>
         <Container>
-          <Navbar.Brand href='#home'>Fern-Herm</Navbar.Brand>
+          <Navbar.Brand href='/'>Fern-Herm</Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='me-auto'>
-              <Nav.Link href='#home'>Home</Nav.Link>
-              <Nav.Link href='#link'>Link</Nav.Link>
+              <Nav.Link href='/jobs'>Job Listings</Nav.Link>
               <NavDropdown title='Dropdown' id='basic-nav-dropdown'>
-                <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
-                <NavDropdown.Item href='#action/3.2'>
-                  Another action
+                <NavDropdown.Item href='/createjob'>Create Job</NavDropdown.Item>
+                <NavDropdown.Item href='/community'>
+                  Community
                 </NavDropdown.Item>
-                <NavDropdown.Item href='#action/3.3'>
-                  Something
+                <NavDropdown.Item href='/profile'>
+                  Profile
                 </NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href='#action/3.4'>
-                  Separated link
+                <NavDropdown.Item href='/chat'>
+                  Chat
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href='/info'>
+                  Info Lookup
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href='/auth/logout'>
+                  Logout
                 </NavDropdown.Item>
               </NavDropdown>
             </Nav>
