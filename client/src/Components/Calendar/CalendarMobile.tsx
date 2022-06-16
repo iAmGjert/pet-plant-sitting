f6d6dce28690dfa9cc91eb564eb5bacaea2fe424
velouriagreen <sammy_soysauce@yahoo.com>
2022-06-16 12:01:05 -0500

    created calendarmobile component. Merging

---
+++ client/src/Components/Calendar/CalendarMobile.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Calendar from 'react-calendar';
+
+import { useSelector } from 'react-redux';
+
+
+
+
+const CalendarMobile = () => {
+  
+  const state = useSelector((state) => state);
+  
+  return (
+
+  )
+
+};
+
+
+export default CalendarMobile;
