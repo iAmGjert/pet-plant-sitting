3102ce76eae500003a14229e97d771eca28118c8
velouriagreen <sammy_soysauce@yahoo.com>
2022-06-16 14:10:43 -0500

    committing to merge Iben's changes

--- client/src/Components/Calendar/Calendar.tsx
+++ client/src/Components/Calendar/Calendar.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 // import PropTypes from 'prop-types';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Calendar from 'react-calendar';
+//import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import Calendar = require('react-calendar-mobile');
 
 import { useSelector } from 'react-redux';
 //import { bindActionCreators } from 'redux'; this is what we will import when we have our actions created
@@ -15,12 +16,19 @@ const CalendarApp = () =>{
   
   return (
     <div>
-      <Calendar onChange={onChange} 
+      {/* <Calendar onChange={onChange} 
         value={value}
       />
       {value.toString()}
   
-    </div>
+    </div> */}
+    <Datepicker
+    controls={['calendar']}
+    inputComponent="input"
+    inputProps={{
+        placeholder: 'Please Select...'
+    }}
+/>
   );
 };
 
