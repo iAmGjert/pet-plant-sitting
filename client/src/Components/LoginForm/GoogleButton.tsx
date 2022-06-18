ad3f32045842c841a9abb8404b51cb70948d52a9
ibenyourbro <iben.oneal@gmail.com>
2022-06-17 20:35:42 -0500

    Fixed Line

--- client/src/Components/LoginForm/GoogleButton.tsx
+++ client/src/Components/LoginForm/GoogleButton.tsx
@@ -1,15 +1,7 @@
-import axios from 'axios';
 import React from 'react';
 import { Button } from 'react-bootstrap';
 
 const GoogleButton = () => {
-  const loginUser = async () => {
-    const user = await axios.get(
-      `${process.env.CLIENT_URL}:${process.env.PORT}/auth/login/success`
-    );
-    console.log(user);
-  };
-
   return (
     <div>
       <Button
