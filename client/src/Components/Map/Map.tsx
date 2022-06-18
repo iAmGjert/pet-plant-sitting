f912aaf0a0eebdb5fb2134903c7d449acbd82e90
Braeden Ford <bford0002@gmail.com>
2022-06-18 12:56:56 -0500

    popup rendering and functional

--- client/src/Components/Map/Map.tsx
+++ client/src/Components/Map/Map.tsx
@@ -1,28 +1,41 @@
-import React, { FC, useState, useEffect } from 'react';
+import React, { FC, useState } from 'react';
 // import { useAppSelector, useAppDispatch } from '../../state/hooks';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import Map, { Marker } from 'react-map-gl';
-// import axios from 'axios';
+import JobPopup from './JobPopup';
+
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface Props {
   user: object
-  userLng: number
-  userLat: number
-  jobsLocations: Array<number>
+  userGeoLoc: Array<number>
+  jobs: Array<object>
+  jobsLocations: any
 }
  
 
 const TOKEN = `${process.env.MAPBOX_TOKEN}`;
 
-const MapComponent: FC<Props> = ({ user, userLng, userLat, jobsLocations }) => {
+const MapComponent: FC<Props> = ({ user, userGeoLoc, jobs, jobsLocations, }) => {
+
+  const [buttonPopup, setButtonPopup] = useState(false);
+  const [jobPopup, setJobPopup] = useState({});
+
+  const showJobInfo = (id) => {
+    for (let i = 0; i < jobs.length; i++) {
+      if (id === jobs[i].id) {
+        setJobPopup(jobs[i]);
+      }
+    }
+    setButtonPopup(!buttonPopup);
+  };
   
   return (
     <div>
       <Map
         initialViewState={{
-          longitude: userLng,
-          latitude: userLat,
+          longitude: userGeoLoc[0],
+          latitude: userGeoLoc[1],
           zoom: 15
         }}
         style={{minHeight: '100vh'}}
@@ -30,8 +43,8 @@ const MapComponent: FC<Props> = ({ user, userLng, userLat, jobsLocations }) => {
         mapboxAccessToken={TOKEN}
       >
         <Marker 
-          longitude={userLng}
-          latitude={userLat}
+          longitude={userGeoLoc[0]}
+          latitude={userGeoLoc[1]}
         >
           <button className='mapMarker' >
             <img src={user.image} alt='X' className='markerPic' />
@@ -40,15 +53,25 @@ const MapComponent: FC<Props> = ({ user, userLng, userLat, jobsLocations }) => {
         {
           jobsLocations.map((job, index) => {
             return <Marker
-              longitude={job[0]}
-              latitude={job[1]}
+              longitude={job[0][0]}
+              latitude={job[0][1]}
               key={`${job}${index}`}
             >
-              <button className='mapMarker'></button>
+              <button className='mapMarker' onClick={() => showJobInfo(job[1])}></button>
             </Marker>;
           })
         }
-      </Map>
+        {
+          jobPopup ?
+            <JobPopup trigger={buttonPopup} setTrigger={showJobInfo}>
+              <h2>{jobPopup.employer_id}</h2>
+              <h3>{`Pets/Plant: ${jobPopup.pet_plant}`}</h3>
+              <h4>{`Start: ${jobPopup.startDate}`}</h4>
+              <h4>{`End: ${jobPopup.endDate}`}</h4>
+              <h5>{`Address: ${jobPopup.location}`}</h5>
+            </JobPopup> : ''
+        }
+      </Map> 
     </div>
   );
 };
--- client/src/Pages/MapMain.tsx
+++ client/src/Pages/MapMain.tsx
@@ -1,7 +1,7 @@
 import React, { FC, useEffect, useState } from 'react';
 import axios from 'axios';
 
-import { useAppSelector, useAppDispatch } from '../state/hooks';
+import { useAppSelector } from '../state/hooks';
 // import { mapActions } from '../state/features/map/mapSlice';
 
 import MapComponent from '../Components/Map/Map';
@@ -15,36 +15,24 @@ const MapMain: FC<Props> = () => {
 
   const user = useAppSelector((state) => state.userProfile.value);
   const jobs = useAppSelector((state) => state.job.jobs);
-  // const userLocation: string = useAppSelector((state) => state.map.userLocation);
-  // const job: object = useAppSelector((state) => state.map.job);
-  // const jobLocation: string = useAppSelector((state) => state.map.jobLocation);
-  // const dispatch = useAppDispatch();
 
-  // const [user, setUser] = useState(null);
-  const [userLng, setUserLng] = useState(null);
-  const [userLat, setUserLat] = useState(null);
+  const [userGeoLoc, setUserGeoLoc] = useState(null);
   const [jobsLocations, setJobsLocations] = useState([]);
-
   
 
   const geoCodeUser = () => {
     axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${user?.location}.json?access_token=${TOKEN}`)
       .then((results) => {
-        // console.log(results, 'HERE');
-        setUserLng(results.data.features[0].center[0]);
-        setUserLat(results.data.features[0].center[1]);
-        // setInitUser(user);
-        // dispatch(mapActions.getUserLocationGeoLng(results.data.features[0].center[0]));
-        // dispatch(mapActions.getUserLocationGeoLat(results.data.features[0].center[1]));
+        setUserGeoLoc(results.data.features[0].center);
       });
   };
 
 
-  const getJobLocations = () => {
-    const mapped = jobs.map(async job => {
+  const geoCodeJobs = () => {
+    const mapped = jobs.map(async (job, id) => {
       const promises = await axios.get(
         `https://api.mapbox.com/geocoding/v5/mapbox.places/${job.location}.json?access_token=${TOKEN}`);
-      return promises.data.features[0].center;
+      return [promises.data.features[0].center, job.id];
     });
     return Promise.all(mapped);
   };
@@ -53,20 +41,24 @@ const MapMain: FC<Props> = () => {
   useEffect(() => {
     if (user && user.location) {
       geoCodeUser();
-      getJobLocations().then((jobs) => {
+      geoCodeJobs().then((jobs) => {
         setJobsLocations(jobs);
       });
     }
   }, [user]);
 
-  // console.log(jobsLocations, 'JOBS');
   
 
   return (
     <div>
       {
-        user && user.location && userLng
-          ? <MapComponent user={user} userLng={userLng} userLat={userLat} jobsLocations={jobsLocations} />
+        user && user.location && userGeoLoc
+          ? <MapComponent 
+            user={user} 
+            userGeoLoc={userGeoLoc}
+            jobs={jobs}
+            jobsLocations={jobsLocations}
+          />
           : 'Loading...'
       }
     </div>
