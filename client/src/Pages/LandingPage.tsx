b53a6e3dd4535e6f3ed74369080de04b008ba9ba
velouriagreen <sammy_soysauce@yahoo.com>
2022-06-20 17:12:56 -0500

    Created scaffolding for LandingPage in accordance with Redux and Typescript

---
+++ client/src/Pages/LandingPage.tsx
@@ -0,0 +1,9 @@
+import React, { FC, useState, useEffect } from 'react';
+import { useAppSelector, useAppDispatch } from '../state/hooks';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface Props {}
+
+const Landing: FC<props> = () => {};
+
+export default LandingPage;
