c3ab6472b39dd3782e1aca7a5999a8dfa66e92a7
royce-reed <rjreed1@gmail.com>
2022-06-20 23:43:27 -0500

    fixed comment loading bug and linked to user profiles from events page

--- client/src/Components/CommunityEvents/AddComment.tsx
+++ client/src/Components/CommunityEvents/AddComment.tsx
@@ -1,40 +1,56 @@
-// import React, { useEffect, useState } from 'react';
-// import Button from 'react-bootstrap/Button';
-// import Modal from 'react-bootstrap/Modal';
-// import { useAppDispatch, useAppSelector } from '../../state/hooks';
-// import { setView } from '../../state/features/events/eventsSlice';
-// import Form from 'react-bootstrap/Form';
-// const AddComment = () => {
-//   const [show, setShow] = useState(false);
+import React, { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import { useAppDispatch, useAppSelector } from '../../state/hooks';
+import { setView } from '../../state/features/events/eventsSlice';
+import Form from 'react-bootstrap/Form';
 
-//   const handleClose = () => setShow(false);
-//   const handleShow = () => setShow(true);
+type Props = {
+  showAddModal: boolean;
+  setShowAddModal: (showAddModal: boolean) => void;
+  handleSubmit: () => void;
+}
 
-//   useEffect(() => {
-//     handleShow();
-//   }, []);
-//   return (
-//     <>
-//       <Modal
-//         show={show}
-//         onHide={handleClose}
-//         backdrop="static"
-//         keyboard={false}
-//       >
-//         <Modal.Body>
-//           <Form>
-//             <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-//               <Form.Label>Example textarea</Form.Label>
-//               <Form.Control as="textarea" rows={3} />
-//             </Form.Group>
-//           </Form>
-//         </Modal.Body>
-//         <Modal.Footer>
-//           <Button variant="primary" >Submit</Button>
-//         </Modal.Footer>
-//       </Modal>
+const AddComment = ({showAddModal, setShowAddModal, handleSubmit}: Props) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleSendComemnt = () => {
+    handleSubmit();
+    setShow(false);
+  };
+
+  useEffect(() => {
+    handleShow();
+  }, []);
+  return (
+    
+    <Modal
+      show={show}
+      onHide={handleClose}
+      backdrop="static"
+      keyboard={false}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>Add Comment</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            {/* <Form.Label>Example textarea</Form.Label> */}
+            <Form.Control as="textarea" rows={3} />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="primary" 
+          onClick={handleSendComemnt}>Submit</Button>
+      </Modal.Footer>
+    </Modal>
    
-//   );
+  );
 
-// };
-// export default AddComment;
+};
+export default AddComment;
