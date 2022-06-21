d95024b84a43f0439ca92e1296e581512840208a
velouriagreen <sammy_soysauce@yahoo.com>
2022-06-20 19:08:01 -0500

    Landing Page is rendering

--- client/src/Pages/LandingPage.tsx
+++ client/src/Pages/LandingPage.tsx
@@ -4,8 +4,18 @@ import { useAppSelector, useAppDispatch } from '../state/hooks';
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface Props {}
 
-const LandingPage: FC<props> = () => {
-  return <h1>Hello</h1>;
+const LandingPage: FC<Props> = () => {
+  const user = useAppSelector((state) => state.userProfile.value);
+  const users = useAppSelector((state) => state.userProfile.users);
+  const petPlants = useAppSelector((state) => state.petPlant.petPlants);
+  const jobs = useAppSelector((state) => state.job.jobs);
+
+  return (
+    <div>
+      <h1>Welcome {user.name ? `, ${user.name}!` : '!'}</h1>
+      <p> Fern Herm is happy to have you!</p>
+    </div>
+  );
 };
 
 export default LandingPage;
