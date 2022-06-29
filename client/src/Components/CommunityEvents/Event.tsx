be05bb679eaa37f433e6af7f9f5606126391963e
royce-reed <rjreed1@gmail.com>
2022-06-29 17:46:29 -0500

    switched front components to use thunk POST request and cleaned code

--- client/src/Components/CommunityEvents/Event.tsx
+++ client/src/Components/CommunityEvents/Event.tsx
@@ -1,19 +1,40 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-// import Row from 'react-bootstrap/Row';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
+import { useAppSelector } from '../../state/hooks';
 
 
 
 const Event = (props: any) =>{
   const { name, location, user, eventObj, switchToDetailsView } = props;
+  const currentUser = useAppSelector(state => state.userProfile.value);
+  // console.log(user);
 
   return (
     <Container>
 
       <Card style={{ width: '95%' }} className='bootstrap-card'>
-        <Card.Header>Host: {user.name}</Card.Header>
+        <Card.Header>
+          <Row>
+            <Col>
+            Host: {user?.name}
+            </Col>
+            {currentUser?.id === user?.id && (  
+              <Col>
+                <Button variant='link'>
+                  edit 
+                </Button>
+                  |
+                <Button variant='link'>
+                  delete
+                </Button>
+              </Col>
+            )}
+          </Row>
+        </Card.Header>
         <Card.Body>
           <Card.Title>{name}</Card.Title>
           <Card.Text>
