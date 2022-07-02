e72bb2d736dfebebbf1bce43a39a76ebf99833a0
royce-reed <rjreed1@gmail.com>
2022-07-02 10:07:49 -0500

    tidy up/remove old comments

--- client/src/Components/LoginForm/Register.tsx
+++ client/src/Components/LoginForm/Register.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Form, Button, Card, Container, Row, Col } from 'react-bootstrap';
-
 import { useNavigate } from 'react-router-dom';
 import { useForm, SubmitHandler } from 'react-hook-form';
+import { Card, Container, Row, Col } from 'react-bootstrap';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 import axios from 'axios';
@@ -17,7 +16,6 @@ type Inputs = {
   location: string;
 };
 
-
 const schema = yup.object().shape({
   firstName: yup.string().required('name is required'),
   lastName: yup.string().required('name is required'),
@@ -25,6 +23,7 @@ const schema = yup.object().shape({
   password: yup.string().min(8).max(20).required('password is required'),
   confirmPassword: yup.string().oneOf([yup.ref('password'), null]),
 });
+
 const Register = () => {
   const navigate = useNavigate();
   const { register, formState: { errors }, handleSubmit } = useForm<Inputs>({
@@ -47,7 +46,6 @@ const Register = () => {
       console.log(error);      
     }
   };
-
 
   return (
     <Container className='Register'><Row><Col><Card>
@@ -90,150 +88,4 @@ const Register = () => {
   );
 };
 
-//   // const { register, handleSubmit, errors } = useForm(/*{ resolver: yupResolver(schema), }*/);
-//   const { register, handleSubmit, formState: { errors } } = useForm();
-//   const onSubmit = (data: any) => console.log(data);
-//   // console.log(errors);
-  
-
-//   
-
 export default Register;
-
-
-
-// const navigate = useNavigate();
-// const [firstName, setFirstName] = useState<string>('');
-// const [lastName, setLastName] = useState<string>('');
-// const [email, setEmail] = useState<string>('');
-// const [password, setPassword] = useState<string>('');
-// // const [passwordConfirm, setPasswordConfirm] = useState<string>('');
-// const [location, setLocation] = useState<string>('');
-
-
-// const registerUser = () => {
-//   axios.post('/auth/local/register', {
-//     name: `${firstName} ${lastName}`,
-//     username: email,
-//     password,
-//     location,
-//   }, {
-//     withCredentials: true,
-//   }).then((res) => {
-//     console.log(res);
-//     res.data.user ? 
-//       axios.post('/auth/local/login', {
-//         username: email,
-//         password,
-//       }, {
-//         withCredentials: true,
-//       }).then((res) => {
-//         console.log(res);
-//         // navigate('/');
-//       }).catch((err) => {
-//         console.log(err);
-//       })
-//       : alert('Failure!');
-//   }).catch((err) => {
-//     console.log(err);
-//   });
-// };
-
-
-
-
-
-// return (
-
-// );
-
-
-// <Form style={{ margin: 'auto', width: '90%' }} >
-//           <Form.Group className='mb-3' controlId='formBasicFirstName'>
-//             <Form.Label>First Name</Form.Label>
-//             <Form.Control 
-//               className='bootstrap-textbox' 
-//!               type="text" 
-//               placeholder="First name" 
-//               name="firstName"
-//               {...register('firstName', { required: true, pattern: /^[a-zA-Z0-9]{3,15}$/i})} />
-//             {errors.firstName && 'First name is required'}
-//           </Form.Group>
-              
-//           <Form.Group className='mb-3' controlId='formBasicLastName'>
-//             <Form.Label>
-//               Last Name
-//             </Form.Label>
-//             <Form.Control 
-//               className='bootstrap-textbox' 
-//!               type="text" 
-//               placeholder="Last name" 
-//               name="lastName"
-//               {...register('lastName', {required: true, pattern: /^[a-zA-Z0-9]{3,15}$/i})} />
-//           </Form.Group>
-            
-//           <Form.Group className='mb-3' controlId='formBasicEmail'>
-//             <Form.Label>
-//               Email address
-//             </Form.Label>
-//             <Form.Control 
-//               className='bootstrap-textbox' 
-//!               type="email" 
-//               placeholder="Email" 
-//               name="email"
-//               {...register('email', {required: true, maxLength: 30, pattern: /.+\@.+\..+/i})} />
-//             <Form.Text className='text-muted'>
-//               Well never share your email with anyone else.
-//             </Form.Text>
-//           </Form.Group>     
-//           <Form.Group className='mb-3' controlId='formBasicPassword'>
-//             <Form.Label>
-//               Password
-//             </Form.Label>
-//             <Form.Control 
-//               className='bootstrap-textbox' 
-//!               type="password" 
-//               placeholder="Password" 
-//               name="password"
-//               {...register('password', {required: true, maxLength: 30, pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/i})} />
-//           </Form.Group>
-           
-//           <Form.Group className='mb-3' controlId='formBasicConfirmPassword'>
-//             <Form.Label>
-//               Confirm Password
-//             </Form.Label>
-//             <Form.Control 
-//!               className='bootstrap-textbox' 
-//               type="password" 
-//               placeholder="Confirm Password" 
-//               name="confirmPassword"
-//               {...register('confirmPassword', {required: true, maxLength: 30, pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/i})} />
-//           </Form.Group>
-              
-//           <Form.Group className='mb-3' controlId='formBasicLocation'>
-//             <Form.Label>
-//               Location
-//             </Form.Label>
-//             <Form.Control 
-//               className='bootstrap-textbox' 
-//!               type='text' 
-//               placeholder='optional' 
-//               name='location'
-//               {...register('location', {required: false, maxLength: 50 })} />
-//           </Form.Group>
-//           <Button 
-//             className='bootstrap-button'
-//             variant='primary' 
-//             // href='/login' 
-//             type="submit" 
-//             id="submit" 
-//             onSubmit={() => /*handleSubmit(onSubmit)*/ console.log('submit')}
-//           >
-//             Submit
-//           </Button>
-//         </Form> 
-
-
-
-
-
