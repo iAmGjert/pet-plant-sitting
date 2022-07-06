a971b27f9bb638c8e3f474b3144ec8e25e089f0a
Eric Gjertsen <gjertsen.eric@gmail.com>
2022-07-06 17:09:21 -0500

    separated components into tabs

--- client/src/Components/JobListings/List.tsx
+++ client/src/Components/JobListings/List.tsx
@@ -7,6 +7,8 @@ import {
   Dropdown,
   Overlay,
   Card,
+  Tab,
+  Tabs,
 } from 'react-bootstrap';
 import moment from 'moment';
 
@@ -14,130 +16,77 @@ const List = ({ setshowapplied, setshowrevoked }) => {
   const target = useRef(null);
   const user = useAppSelector((state) => state.userProfile.value);
   const jobs = useAppSelector((state) => state.job.jobs);
-  const [view, setView] = useState('Available Jobs');
-
   return (
     <>
-      <DropdownButton
-        ref={target}
-        as={ButtonGroup}
-        title={view}
-        id="bg-nested-dropdown"
+      <Tabs
+        defaultActiveKey="profile"
+        id="uncontrolled-tab-example"
+        className="mb-3"
       >
-        <Dropdown.Item
-          onClick={(e) => {
-            setView(e.target.textContent);
-          }}
-          eventKey="1"
-        >
-          Available Jobs
-        </Dropdown.Item>
-        <Dropdown.Item
-          onClick={(e) => {
-            setView(e.target.textContent);
-          }}
-          eventKey="2"
-        >
-          My Jobs
-        </Dropdown.Item>
-        <Dropdown.Item
-          onClick={(e) => {
-            setView(e.target.textContent);
-          }}
-          eventKey="2"
-        >
-          My Applications
-        </Dropdown.Item>
-      </DropdownButton>
-      {view === 'Available Jobs' && Array.isArray(jobs) ? (
-        user?.name !== '' ? (
-          jobs
-            .filter((job) => {
-              if (job.employer_id !== user.id) {
-                for (let i = 0; i < job.job_applicants.length; i++) {
-                  if (job.job_applicants[i].user_id === user.id) {
-                    return false;
-                  }
-                }
-                if (job.sitter_id === null) {
-                  return true;
-                }
-              }
-              return false;
-            })
-            .filter((job) => {
-              if (moment(job.endDate).diff(moment(), 'days') < 0) {
-                return false;
-              }
-              return true;
-            }).length > 0 ? (
-              jobs
-                .filter((job) => {
-                  if (job.employer_id !== user.id) {
-                    for (let i = 0; i < job.job_applicants.length; i++) {
-                      if (job.job_applicants[i].user_id === user.id) {
-                        return false;
-                      }
-                    }
-                    if (job.sitter_id === null) {
-                      return true;
-                    }
-                  }
-                  return false;
-                })
-                .filter((job) => {
-                  if (moment(job.endDate).diff(moment(), 'days') < 0) {
-                    return false;
-                  }
-                  return true;
-                })
-                .map((job, index) => {
-                  return (
-                    <Job
-                      setshowrevoked={setshowrevoked}
-                      key={`job#${index}`}
-                      setshowapplied={setshowapplied}
-                      job={job}
-                    />
-                  );
-                })
-            ) : (
-              <Card>
-                <Card.Title>No Jobs to Display!</Card.Title>
-              </Card>
-            )
-        ) : (
-          jobs
-            .filter((job) => {
-              if (moment(job.endDate).diff(moment(), 'days') < 0) {
-                return false;
-              }
-              if (job.sitter_id !== null) {
-                return false;
-              }
-              return true;
-            })
-            .map((job, index) => {
-              return (
-                <Job
-                  setshowrevoked={setshowrevoked}
-                  key={`job#${index}`}
-                  setshowapplied={setshowapplied}
-                  job={job}
-                />
-              );
-            })
-        )
-      ) : view === 'My Jobs' && Array.isArray(jobs) && user.name !== '' ? (
-        jobs.filter((job) => {
-          if (job.employer_id !== user.id) {
-            return false;
-          }
-          return true;
-        }).length > 0 ? (
+        <Tab eventKey="home" title="Avaiable Jobs">
+          {user?.name !== '' ? (
             jobs
               .filter((job) => {
                 if (job.employer_id !== user.id) {
+                  for (let i = 0; i < job.job_applicants.length; i++) {
+                    if (job.job_applicants[i].user_id === user.id) {
+                      return false;
+                    }
+                  }
+                  if (job.sitter_id === null) {
+                    return true;
+                  }
+                }
+                return false;
+              })
+              .filter((job) => {
+                if (moment(job.endDate).diff(moment(), 'days') < 0) {
+                  return false;
+                }
+                return true;
+              }).length > 0 ? (
+                jobs
+                  .filter((job) => {
+                    if (job.employer_id !== user.id) {
+                      for (let i = 0; i < job.job_applicants.length; i++) {
+                        if (job.job_applicants[i].user_id === user.id) {
+                          return false;
+                        }
+                      }
+                      if (job.sitter_id === null) {
+                        return true;
+                      }
+                    }
+                    return false;
+                  })
+                  .filter((job) => {
+                    if (moment(job.endDate).diff(moment(), 'days') < 0) {
+                      return false;
+                    }
+                    return true;
+                  })
+                  .map((job, index) => {
+                    return (
+                      <Job
+                        setshowrevoked={setshowrevoked}
+                        key={`job#${index}`}
+                        setshowapplied={setshowapplied}
+                        job={job}
+                      />
+                    );
+                  })
+              ) : (
+                <Card>
+                  <Card.Title>No Jobs to Display!</Card.Title>
+                </Card>
+              )
+          ) : (
+            jobs
+              .filter((job) => {
+                if (moment(job.endDate).diff(moment(), 'days') < 0) {
+                  return false;
+                }
+                if (job.sitter_id !== null) {
                   return false;
                 }
                 return true;
@@ -146,57 +95,93 @@ const List = ({ setshowapplied, setshowrevoked }) => {
                 return (
                   <Job
                     setshowrevoked={setshowrevoked}
-                    setshowapplied={setshowapplied}
                     key={`job#${index}`}
+                    setshowapplied={setshowapplied}
                     job={job}
                   />
                 );
               })
-          ) : (
-            <Card>
-              <Card.Title>No Jobs to Display!</Card.Title>
-            </Card>
-          )
-      ) : view === 'My Applications' &&
-        Array.isArray(jobs) &&
-        user.name !== '' ? (
-          jobs.filter((job) => {
-            for (let i = 0; i < job.job_applicants.length; i++) {
-              if (job.job_applicants[i].user_id === user.id) {
-                return true;
-              }
-            }
-            return false;
-          }).length > 0 ? (
-              jobs
-                .filter((job) => {
-                  for (let i = 0; i < job.job_applicants.length; i++) {
-                    if (job.job_applicants[i].user_id === user.id) {
-                      return true;
-                    }
-                  }
+          )}
+        </Tab>
+        <Tab eventKey="profile" title="My Jobs">
+          {
+            user.name !== '' ? (
+              jobs.filter((job) => {
+                if (job.employer_id !== user.id) {
                   return false;
-                })
-                .map((job, index) => {
-                  return (
-                    <Job
-                      setshowrevoked={setshowrevoked}
-                      key={`job#${index}`}
-                      setshowapplied={setshowapplied}
-                      job={job}
-                    />
-                  );
-                })
+                }
+                return true;
+              }).length > 0 ? (
+                  jobs
+                    .filter((job) => {
+                      if (job.employer_id !== user.id) {
+                        return false;
+                      }
+                      return true;
+                    })
+                    .map((job, index) => {
+                      return (
+                        <Job
+                          setshowrevoked={setshowrevoked}
+                          setshowapplied={setshowapplied}
+                          key={`job#${index}`}
+                          job={job}
+                        />
+                      );
+                    })
+                ) : (
+                  <Card>
+                    <Card.Title>No Jobs to Display!</Card.Title>
+                  </Card>
+                )
             ) : (
               <Card>
-                <Card.Title>No Jobs to Display!</Card.Title>
+                <Card.Title>Login to view these jobs!</Card.Title>
               </Card>
             )
-        ) : (
-          <Card>
-            <Card.Title>Login to view these jobs!</Card.Title>
-          </Card>
-        )}
+          }
+        </Tab>
+        <Tab eventKey="contact" title="My Applications">
+          {user.name !== '' ? (
+            jobs.filter((job) => {
+              for (let i = 0; i < job.job_applicants.length; i++) {
+                if (job.job_applicants[i].user_id === user.id) {
+                  return true;
+                }
+              }
+              return false;
+            }).length > 0 ? (
+                jobs
+                  .filter((job) => {
+                    for (let i = 0; i < job.job_applicants.length; i++) {
+                      if (job.job_applicants[i].user_id === user.id) {
+                        return true;
+                      }
+                    }
+                    return false;
+                  })
+                  .map((job, index) => {
+                    return (
+                      <Job
+                        setshowrevoked={setshowrevoked}
+                        key={`job#${index}`}
+                        setshowapplied={setshowapplied}
+                        job={job}
+                      />
+                    );
+                  })
+              ) : (
+                <Card>
+                  <Card.Title>No Jobs to Display!</Card.Title>
+                </Card>
+              )
+          ) : (
+            <Card>
+              <Card.Title>Login to view these jobs!</Card.Title>
+            </Card>
+          )}
+        </Tab>
+      </Tabs>
     </>
   );
 };
