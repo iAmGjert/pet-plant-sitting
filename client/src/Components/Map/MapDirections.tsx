bb215d6fbc5ed06b1cfe194ef3fd731dfaa6e601
Braeden Ford <bford0002@gmail.com>
2022-07-11 14:32:58 -0500

    refactored directions to own component

--- client/src/Components/Map/MapDirections.tsx
+++ client/src/Components/Map/MapDirections.tsx
@@ -1,14 +1,56 @@
-import axios from 'axios';
-import React from 'react';
-import { Button } from 'react-bootstrap';
+import React, { FC } from 'react';
+import { Button, ListGroup } from 'react-bootstrap';
+import { Layer, Source } from 'react-map-gl';
 
 
-const TOKEN = `${process.env.MAPBOX_TOKEN}`;
+interface Props {
+  steps: any
+  dirCoordinates: Array<number>
+  cancelNav: boolean
+  setCancelNav: any
+}
 
-const MapDirections = () => {
+const MapDirections: FC<Props> = ({ steps, dirCoordinates, cancelNav, setCancelNav }) => {
+
+  const directions = {
+    type: 'Feature',
+    properties: {},
+    geometry: {
+      type: 'LineString',
+      coordinates: dirCoordinates
+    }
+  };
 
   return (
-    <Button className='bootstrap-button' style={{position: 'absolute'}}>hey</Button>
+    <div>
+      {
+        cancelNav &&
+        <><Source id="polylineLayer" type="geojson" data={directions}>
+          <Layer
+            id="lineLayer"
+            type="line"
+            source="my-data"
+            layout={{
+              'line-join': 'round',
+              'line-cap': 'round'
+            }}
+            paint={{
+              'line-color': 'darkblue',
+              'line-width': 7
+            }} />
+        </Source><ListGroup variant='flush' as='ol' className='step-instructions' numbered>
+          <Button className='bootstrap-button' onClick={() => setCancelNav(!cancelNav)}>End Route</Button>
+          {steps.map((step, i) => {
+            return <ListGroup.Item as='li'
+              key={`${step}${i}`}
+              className='step-instructions-card'
+            >
+              {step.maneuver.instruction}
+            </ListGroup.Item>;
+          })}
+        </ListGroup></>
+      }
+    </div>
   );
 };
 
