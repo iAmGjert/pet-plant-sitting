87da903b9e06102c5c605df3034db6a9b4a78b1f
ibenyourbro <iben.oneal@gmail.com>
2022-07-13 10:18:03 -0500

    Added more Space to Bios

--- database/models/UserModel.ts
+++ database/models/UserModel.ts
@@ -3,7 +3,8 @@ const DataTypes = require('sequelize').DataTypes;
 export const UserModel = {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   name: DataTypes.STRING,
-  username: { // <-- may also be referered to as 'email'
+  username: {
+    // <-- may also be referered to as 'email'
     type: DataTypes.STRING,
     unique: true,
   },
@@ -12,9 +13,9 @@ export const UserModel = {
   location: DataTypes.STRING,
   sitter_rating: DataTypes.FLOAT,
   total_sitter_ratings: DataTypes.INTEGER,
-  bio: DataTypes.STRING(1234),
+  bio: DataTypes.STRING(2000),
   average_rating: { type: DataTypes.FLOAT, defaultValue: 5 },
   total_ratings: DataTypes.INTEGER,
   gallery_id: DataTypes.INTEGER,
-  theme: DataTypes.STRING
+  theme: DataTypes.STRING,
 };
