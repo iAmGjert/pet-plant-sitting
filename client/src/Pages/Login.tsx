cac48f1ac084a69767f7a3d986083b019b7861a8
royce-reed <rjreed1@gmail.com>
2022-07-13 11:02:25 -0500

    added styling to login form and added logo to nav bar

--- client/src/Pages/Login.tsx
+++ client/src/Pages/Login.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import { Col, Row, Container } from 'react-bootstrap';
-import GoogleButton from '../Components/LoginForm/GoogleButton';
+import Google from '../Components/LoginForm/GoogleButton';
 import LoginForm from '../Components/LoginForm/LoginForm';
 
 // type Props = {};
 
 const Login = () => {
   return (
-    <Container fluid className='vh-100 '>
+    <Container fluid className='vh-100 login-container' >
       <Row className='align-items-center' xs={1} md={1}>
         <Col xs={{ order: 'last' }}>
-          <GoogleButton />
+          <Google />
         </Col>
         <Col xs={{ order: 1 }} md={{ order: 1 }}>
           <LoginForm />
+        </Col>
+        <Col xs={{ order: 2 }} md={{ order: 2 }}>
+          <div className='seperator'>
+
+            <div className='space'></div>or<div className='space'></div>
+          </div>
         </Col>
       </Row>
     </Container>
