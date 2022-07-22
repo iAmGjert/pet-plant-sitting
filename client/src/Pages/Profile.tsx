b3d0cc522564488f0de2660338755be913173d7b
ibenyourbro <iben.oneal@gmail.com>
2022-07-22 10:37:13 -0500

    Added padding to container

--- client/src/Pages/Profile.tsx
+++ client/src/Pages/Profile.tsx
@@ -260,7 +260,7 @@ const Profile = () => {
     getLocation(profileUser?.location);
   }, [profileUser]);
   return (
-    <Container>
+    <Container style={{ height: '100%', paddingBottom: '18%' }}>
       {completeProfile !== 6 && editable && (
         <ToastContainer position='middle-center' style={{ zIndex: 2 }}>
           <Toast
