ef2b66b6f18cc791ae4c8a454b648d1c727bf127
velouriagreen <sammy_soysauce@yahoo.com>
2022-07-26 09:23:01 -0500

    Deleted bacon ipsum and added user friendly bacon comments in community events comments

--- client/src/Pages/CommunityEventsMain.tsx
+++ client/src/Pages/CommunityEventsMain.tsx
@@ -1,6 +1,13 @@
 import React, { ReactNode, useEffect } from 'react';
 import { useAppSelector, useAppDispatch } from '../state/hooks';
-import { setView, setEventObj, selectAllEvents, getEventsStatus, fetchEvents, pageView } from '../state/features/events/eventsSlice';
+import {
+  setView,
+  setEventObj,
+  selectAllEvents,
+  getEventsStatus,
+  fetchEvents,
+  pageView,
+} from '../state/features/events/eventsSlice';
 import Event from '../Components/CommunityEvents/Event';
 import Details from '../Components/CommunityEvents/Details';
 import CreateEvent from '../Components/CommunityEvents/CreateEvent';
@@ -11,7 +18,6 @@ import { EventTYPE } from '../Components/CommunityEvents/types/types';
 import EditEvent from '../Components/CommunityEvents/EditEvent';
 import { ArrowLeft } from 'react-bootstrap-icons';
 // import { PencilSquare } from 'react-bootstrap-icons';
-
 
 const CommunityEventsMain = () => {
   const dispatch = useAppDispatch();
@@ -24,7 +30,7 @@ const CommunityEventsMain = () => {
       dispatch(fetchEvents());
     }
   }, [eventsStatus, dispatch]);
-  
+
   const changeView = (option: string) => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
     dispatch(setView(option));
@@ -33,17 +39,26 @@ const CommunityEventsMain = () => {
     changeView('details');
     dispatch(setEventObj(eventObj));
   };
-   
+
   type events = EventTYPE[];
   const renderView = (): ReactNode | null => {
     if (view === 'list') {
-      const orderedEvents = events instanceof Array && events.slice()
-        .sort((a, b) => a.startDate.localeCompare(b.startDate))
-        .filter((event) => new Date() <= new Date(`${event.startDate} ${event.startTime}`));
+      const orderedEvents =
+        events instanceof Array &&
+        events
+          .slice()
+          .sort((a, b) => a.startDate.localeCompare(b.startDate))
+          .filter(
+            (event) =>
+              new Date() <= new Date(`${event.startDate} ${event.startTime}`)
+          );
 
       return orderedEvents.map((event: EventTYPE, index: number) => (
-        <React.Fragment key={`Event key: ${~~(Math.random() * 10000) * (event.id * index)}`} >
-          <Event className='events-list' 
+        <React.Fragment
+          key={`Event key: ${~~(Math.random() * 10000) * (event.id * index)}`}
+        >
+          <Event
+            className='events-list'
             name={event.name}
             host={event.host}
             location={event.location}
@@ -66,50 +81,61 @@ const CommunityEventsMain = () => {
     }
   };
   // console.log(view);
-  
-  const event = useAppSelector(state => state.events.event);
+
+  const event = useAppSelector((state) => state.events.event);
 
   return (
     <Container fluid>
-      <div className="main-text">
-        <h1>Community Events</h1>
+      <div className='main-text'>
+        <h1>Welcome to Community Events</h1>
       </div>
-      { view === 'list' ? 
-        <div className="events-create-btn">
-          <Button className='bootstrap-button' onClick={() => changeView('create-event')} size='sm'>
-          Create Event</Button>
+      {view === 'list' ? (
+        <div className='events-create-btn'>
+          <Button
+            className='bootstrap-button'
+            onClick={() => changeView('create-event')}
+            size='sm'
+          >
+            Create Event
+          </Button>
         </div>
-        : view === 'create-event' ? 
-          <div className="events-create-btn">
-            <Button className='bootstrap-button' onClick={() => changeView('list')} size='sm'>
-              <ArrowLeft/>  Back
-            </Button>
-          </div> 
-          : view === 'details' ? 
-            <div className="events-create-btn">
-              <Button className='bootstrap-button' variant="primary" size='sm'
-                onClick={() => changeView('list')} >
-                <ArrowLeft /> Back to Events
-              </Button>
-            </div> 
-            : view === 'edit-event' ?
-              <div className="events-create-btn">
-                <Button className='bootstrap-button' variant="primary" size='sm'
-                  onClick={() => changeView('details')} >
-                  <ArrowLeft /> Back to {event.name}
-                </Button>
-               
-              </div>
-              : null
-
-      }
-
-
+      ) : view === 'create-event' ? (
+        <div className='events-create-btn'>
+          <Button
+            className='bootstrap-button'
+            onClick={() => changeView('list')}
+            size='sm'
+          >
+            <ArrowLeft /> Back
+          </Button>
+        </div>
+      ) : view === 'details' ? (
+        <div className='events-create-btn'>
+          <Button
+            className='bootstrap-button'
+            variant='primary'
+            size='sm'
+            onClick={() => changeView('list')}
+          >
+            <ArrowLeft /> Back to Events
+          </Button>
+        </div>
+      ) : view === 'edit-event' ? (
+        <div className='events-create-btn'>
+          <Button
+            className='bootstrap-button'
+            variant='primary'
+            size='sm'
+            onClick={() => changeView('details')}
+          >
+            <ArrowLeft /> Back to {event.name}
+          </Button>
+        </div>
+      ) : null}
 
       <div className='container'>{renderView()}</div>
     </Container>
   );
 };
-
 
 export default CommunityEventsMain;
